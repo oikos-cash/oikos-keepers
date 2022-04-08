b0ceb87a2fe514d43c35a35c7f61ccf44dc68d1b
oikos1 <albert@oikos.cash>
2022-04-08 17:23:50 -0400

    simplify contract

--- contracts/RewardsRecharger.sol
+++ contracts/RewardsRecharger.sol
@@ -2,11 +2,13 @@
 pragma solidity 0.8.13;
 
 import "@chainlink/contracts/src/v0.8/interfaces/KeeperCompatibleInterface.sol";
-import "./CakePool.sol";
-import "./Ownable.sol";
 
+
+interface ICakePool {
+     function notifyRewardAmount(uint256 reward) external ;
+}
  
-contract RewardsRecharger is KeeperCompatibleInterface, Ownable, CakePool {
+contract RewardsRecharger is KeeperCompatibleInterface {
    
     address public derive_oUSD_rewards_contract      = 0x8559AeF12e2C40E66f0eb600d0Dd8ae5CeA419D7;
     address public pancake_oUSD_BNB_rewards_contract = 0xD8f27e3e7da60d8CF43b76e335412369FEd96295;
@@ -16,21 +18,13 @@ contract RewardsRecharger is KeeperCompatibleInterface, Ownable, CakePool {
     uint public pancake_oUSD_BNB_rewards_amount = 100000 ether;
     uint public derive_DRV_rewards_amount = 300000 ether;
 
-    uint public interval;
-    uint public lastTimestamp;
+    uint public interval = 604800; // 1 week
+    uint public lastTimestamp = block.timestamp;
 
-    CakePool public derivePool_oUSD ;
-    CakePool public pancakePool_oUSD_BNB ;
-    CakePool public derivePool_DRV;
+    ICakePool public derivePool_oUSD      = ICakePool(derive_oUSD_rewards_contract);
+    ICakePool public pancakePool_oUSD_BNB = ICakePool(pancake_oUSD_BNB_rewards_contract);
+    ICakePool public derivePool_DRV       = ICakePool(derive_DRV_rewards_contract);
 
-    /* ---------- Constructor ---------- */
-    constructor(uint256 updateInterval) public Ownable() {
-        interval = updateInterval;
-        lastTimestamp = block.timestamp;
-        derivePool_oUSD = CakePool(derive_oUSD_rewards_contract);
-        pancakePool_oUSD_BNB = CakePool(pancake_oUSD_BNB_rewards_contract);
-        derivePool_DRV = CakePool(derive_DRV_rewards_contract);
-    }
 
     //Called by Chainlink Keepers to check if work needs to be done
     function checkUpkeep(
